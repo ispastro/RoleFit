58b095b6d52bbf46ee7a11c46c93b40d0af15aef
Haile Asaye <haileasaye51@gmail.com>
2026-04-16 15:31:27 +0300

    feat: add LaTeX generator to rebuild tailored resume

---
+++ backend/latex_generator.py
@@ -0,0 +1,16 @@
+class LaTeXGenerator:
+    def __init__(self, original_tex_path):
+        with open(original_tex_path, 'r', encoding='utf-8') as f:
+            self.template = f.read()
+    
+    def replace_section(self, section_name, new_content):
+        """Replace a section in the LaTeX template with new content"""
+        import re
+        pattern = rf'(\\section\{{{section_name}\}})(.*?)(?=\\section|\\end\{{document\}})'
+        replacement = rf'\1\n{new_content}\n'
+        self.template = re.sub(pattern, replacement, self.template, flags=re.DOTALL)
+    
+    def generate(self, output_path):
+        """Write the tailored resume to a new file"""
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(self.template)
